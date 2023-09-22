78dfda58277241be099879cc203f1fc278898811
Jack Freebairn <144003627+freebaj10@users.noreply.github.com>
2023-09-22 12:19:56 +0100

    Initial commit

---
+++ IntroMac/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IntroMac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Mac users");
+        }
+    }
+}
